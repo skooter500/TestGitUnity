e96a3945179a0035ed3dbe7c6b6151b3791661a9
Bryan Duggan <skooter500@gmail.com>
2018-03-08 09:46:28 +0000

    Bryan's changes

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -7,7 +7,7 @@ public class CameraControl : MonoBehaviour {
     // Happy mothers day
 	// Use this for initialization
 	void Start () {
-		
+        Vector3 v = Vector3.zero;
 	}
 	
 	// Update is called once per frame
